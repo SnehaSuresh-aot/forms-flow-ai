cb30479e2b8b759be505c28c212c15bbb6a78e2d
Sneha Suresh <sneha.suresh@aot-technologies.com>
2024-02-21 09:50:50 +0530

    connector implementation for email connector

---
+++ forms-flow-bpm/forms-flow-bpm-camunda/src/main/java/org/camunda/bpm/extension/hooks/adapter/response/EmptyResponse.java
@@ -0,0 +1,20 @@
+package org.camunda.bpm.extension.hooks.adapter.response;
+
+import org.camunda.connect.spi.ConnectorResponse;
+
+import java.util.Collections;
+import java.util.Map;
+
+public class EmptyResponse implements ConnectorResponse {
+
+    @Override
+    public Map<String, Object> getResponseParameters() {
+        return Collections.emptyMap();
+    }
+
+    @Override
+    public <V> V getResponseParameter(String name) {
+        return null;
+    }
+
+}
